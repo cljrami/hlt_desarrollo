9c7ccf9a9388315a1f69e0aef02aad6d41475e53
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2023-12-27 18:29:50 -0300

    relacion uno a mucho en modelos
    Se ha realizdo la relacion uno a muchos en los modelos de usuarios, post y categorias

--- app/Livewire/Registro/Registro.php
+++ app/Livewire/Registro/Registro.php
@@ -3,11 +3,41 @@
 namespace App\Livewire\Registro;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class Registro extends Component
 {
+    public $pseudonimo;
+    public $fecha_nacimiento;
+    public $email;
+    public $password;
+    public $repassword;
+
+    protected $rules = [
+        'pseudonimo' => 'required|min:4|max:15',
+        'fecha_nacimiento' => 'required',
+        'email' => 'required|min:12|max:50',
+        'password' => 'required|min:8|max:15'
+    ];
+
     public function render()
     {
         return view('livewire.registro.registro');
+    }
+
+    public function login(){
+        try{
+            $this->validate();
+            User::create([
+                'name' => strtoupper($this->pseudonimo),
+                'fecha_nacimiento' => $this->fecha_nacimiento,
+                'email' => strtoupper($this->email),
+                'password' => Hash::make($this->password)
+            ]);
+            return redirect()->route('ingreso');
+        }catch(Exception $error){
+            dd($error);
+        }
     }
 }
--- database/migrations/2023_12_27_130623_create_posts_table.php
+++ database/migrations/2023_12_27_130623_create_posts_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->date('fecha_nacimiento');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('descripcion');
+            $table->text('post');
+            $table->foreignId('categoria_id')->references('id')->on('categorias')->onDelete('CASCADE');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('CASCADE');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
